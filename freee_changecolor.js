11be0472fad2e5cef14fdb2032b0360b73704d73
m-haketa <git@excelspeedup.com>
2019-10-17 06:49:31 +0900

    スタイル一覧をオブジェクトの配列として切り出し

--- freee_changecolor.js
+++ freee_changecolor.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Freee_ChangeColor
 // @namespace    http://gas.excelspeedup.com/
-// @version      0.1
+// @version      0.2
 // @description  freeeの色を変える
 // @author       m-haketa
 // @match        https://secure.freee.co.jp/*
@@ -10,31 +10,36 @@
 
 (function() {
   "use strict";
+  var getClass = function (string) { return document.getElementsByClassName(string) };
+
+  var changeStyleList = [
+    {
+      elements: getClass("tags-combobox__tagify-tag--partner"),
+      stylename: "backgroundColor",
+      value: "#f5dede"
+    },
+    {
+      elements: getClass("expense"),
+      stylename: "backgroundColor",
+      value: "#f5dede"
+    },
+    {
+      elements: getClass("tagify-tag-bg-partner"),
+      stylename: "backgroundColor",
+      value: "#f5dede"
+    }
+  ];
 
   function setStyle(elements, stylename, value) {
     for (var i = 0; i < elements.length; i++) {
       elements[i].style[stylename] = value;
-      console.log(i);
     }
   }
 
   function changeColor() {
-    setStyle(
-      document.getElementsByClassName("tags-combobox__tagify-tag--partner"),
-      "backgroundColor",
-      "#f5dede"
-    );
-    setStyle(
-      document.getElementsByClassName("expense"),
-      "backgroundColor",
-      "#f5dede"
-    );
-    setStyle(
-      document.getElementsByClassName("tagify-tag-bg-partner"),
-      "backgroundColor",
-      "#f5dede"
-    );
-    console.log("test");
+    changeStyleList.forEach(function(styleData) {
+      setStyle(styleData.elements, styleData.stylename, styleData.value);
+    });
   }
 
   document.addEventListener("click", changeColor);
